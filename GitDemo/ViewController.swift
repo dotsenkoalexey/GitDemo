4b45e1a9cc9f3da0dd2b4a1d6624cea89611e645
Alexey Dotsenko <dotsenkoalexey@yandex.ru>
2020-04-13 23:40:44 +0300

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Алексей Доценко on 13.04.2020.
+//  Copyright © 2020 Алексей Доценко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
